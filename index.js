f3fff2205d8a69b3bf9a775aa7747ab5f605c879
Jason Amador <jason.amador@gmail.com>
2017-12-08 11:20:57 -0600

    knex config file, express skeleton in index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const config = require('./knexfile')['development'];
+const knex = require('knex')(config);
+
+const port = process.env.PORT || 8000;
+
+const app = express();
+
+app.listen(port, () => {
+  console.log('listening on ', port);
+})
