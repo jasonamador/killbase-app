5d8e780174e4a023b066500e4f1b2b35f99f745f
Jason Amador <jason.amador@gmail.com>
2017-12-11 07:55:37 -0600

    clients

---
+++ seeds/04-clients.js
@@ -0,0 +1,29 @@
+exports.seed = function(knex, Promise) {
+  const fs = require('fs');
+
+  // get targets from contracts file
+  let contractsCsv = fs.readFileSync('./data/contracts.csv', 'UTF-8');
+
+  // clean it
+  let contracts = contractsCsv.trim().replace(/"/g, '').split('\n').map((e) => e.split(', '));
+  contracts.shift(1);
+
+  let peopleNamesToIds = {};
+
+  return knex('people')
+    .select()
+    .then((result) => {
+      result.forEach((e) => {
+        peopleNamesToIds[e.name] = e.id;
+      });
+
+      let clients = [];
+      for (let i = 0; i < contracts.length; i++) {
+        let client = {};
+        client['person_id'] = peopleNamesToIds[contracts[i][4]];
+        clients.push(client);
+      }
+
+      return knex('clients').insert(clients);
+    })
+  };
