9f595acc143b3664426b08d101aba06ea2d157a2
Jason Amador <jason.amador@gmail.com>
2017-12-11 11:40:07 -0600

    get assassin

--- index.js
+++ index.js
@@ -32,6 +32,32 @@ app.get('/assassins', (req, res) => {
     });
 });
 
+app.get('/assassin/:id', (req, res) => {
+  knex('assassins').select('assassins.id', 'people.name', 'assassins.weapon', 'assassins.contact_info as contactInfo', 'assassins.price', 'assassins.rating', 'assassins.kills', 'assassins.age')
+    .leftJoin('people', 'assassins.person_id', 'people.id')
+    .where('assassins.id', req.params.id)
+    .then((assassins) => {
+      knex('code_names').select().then((codeNamesDb) => {
+        let codeNames = {};
+        codeNamesDb.forEach((codeName) => {
+          if (!codeNames[codeName.assassin_id]) {
+            codeNames[codeName.assassin_id] = [];
+          }
+          codeNames[codeName.assassin_id].push(codeName.code_name);
+        });
+        assassins.forEach((assassin) => {
+          if (codeNames[assassin.id]) {
+            assassin.codeNames = codeNames[assassin.id];
+          } else {
+            assassin.codeNames = [];
+          }
+        });
+        res.send(assassins);
+      });
+    });
+
+});
+
 app.listen(port, () => {
   console.log('listening on ', port);
 })
