e4e00f432f3fa1f6deb9eed889823a3cf045a303
Jason Amador <jason.amador@gmail.com>
2017-12-14 18:31:48 -0600

    routes for complete and incomplete filtering for contracts

--- routes/contracts.js
+++ routes/contracts.js
@@ -49,6 +49,93 @@ router.get('/', (req, res) => {
   });
 });
 
+router.get('/incomplete', (req, res) => {
+  let clientIdsToNames = {};
+  let targetIdsToNames = {};
+  knex('contracts').select().where('complete', 'false')
+  .then((contracts) => {
+    knex('clients').select('people.name', 'clients.id')
+      .leftJoin('people', 'people.id', 'clients.person_id')
+      .then((clients) => {
+        clients.forEach((e) => {
+          clientIdsToNames[e.id] = e.name;
+        });
+        contracts.forEach((e) => {
+          e.clientName = clientIdsToNames[e.client_id];
+        });
+      })
+      .then(() => {
+        knex('targets').select('people.name', 'targets.id', 'people.photo_url', 'targets.location', 'targets.security')
+          .leftJoin('people', 'people.id', 'targets.person_id').then((targets) => {
+              targets.forEach((e) => {
+                targetIdsToNames[e.id] = e.name;
+              });
+              contracts.forEach((e) => {
+                e.targetName = targetIdsToNames[e.target_id];
+              });
+            })
+            .then(() => {
+              res.render('contracts/list', {contracts});
+            })
+            .catch((e) => {
+              console.error(e);
+              res.sendStatus(500);
+            })
+      })
+      .catch((e) => {
+        console.error(e);
+        res.sendStatus(500);
+      });
+  })
+  .catch((e) => {
+    console.error(e);
+    res.sendStatus(500);
+  });
+});
+
+router.get('/complete', (req, res) => {
+  let clientIdsToNames = {};
+  let targetIdsToNames = {};
+  knex('contracts').select().where('complete', 'true')
+  .then((contracts) => {
+    knex('clients').select('people.name', 'clients.id')
+      .leftJoin('people', 'people.id', 'clients.person_id')
+      .then((clients) => {
+        clients.forEach((e) => {
+          clientIdsToNames[e.id] = e.name;
+        });
+        contracts.forEach((e) => {
+          e.clientName = clientIdsToNames[e.client_id];
+        });
+      })
+      .then(() => {
+        knex('targets').select('people.name', 'targets.id', 'people.photo_url', 'targets.location', 'targets.security')
+          .leftJoin('people', 'people.id', 'targets.person_id').then((targets) => {
+              targets.forEach((e) => {
+                targetIdsToNames[e.id] = e.name;
+              });
+              contracts.forEach((e) => {
+                e.targetName = targetIdsToNames[e.target_id];
+              });
+            })
+            .then(() => {
+              res.render('contracts/list', {contracts});
+            })
+            .catch((e) => {
+              console.error(e);
+              res.sendStatus(500);
+            })
+      })
+      .catch((e) => {
+        console.error(e);
+        res.sendStatus(500);
+      });
+  })
+  .catch((e) => {
+    console.error(e);
+    res.sendStatus(500);
+  });
+});
 router.get('/:id', (req, res) => {
 
 });
