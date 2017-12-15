5090a71bd8046b051ba3b9b84a09cb88cc637e95
Jason Amador <jason.amador@gmail.com>
2017-12-15 14:06:41 -0600

    new contract form and ajax

--- public/js/front.js
+++ public/js/front.js
@@ -29,6 +29,26 @@ $(() => {
     });
   });
 
+  $('#postNewContract').on('click', (e) => {
+    e.preventDefault();
+    let array = $('#createContractForm').serializeArray();
+    let contract = {};
+    array.forEach((e) => {
+      contract[e.name] = e.value;
+    });
+    // should probably validate this data
+    $.ajax({
+      url: '/contracts',
+      type: '/post',
+      contentType: 'application/json',
+      data: JSON.stringify(contract),
+      dataType: 'json'
+    })
+    .then((contract) => {
+      window.location.href = '/contracts';
+    });
+  });
+
   $('#postNewAssassin').on('click', (e) => {
     e.preventDefault();
     let array = $('#createAssassinForm').serializeArray();
@@ -45,8 +65,7 @@ $(() => {
       dataType: 'json'
     })
     .then((assassin) => {
-      console.log("SUCCESS");
-      window.location.href = "/assassins";
+      window.location.href = '/assassins';
     })
     .catch((e) => {
       console.log(e);
